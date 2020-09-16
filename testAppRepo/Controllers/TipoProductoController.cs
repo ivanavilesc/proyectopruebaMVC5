ea0d8f3e931cbbb3b87da2364484c24da1855a1c
ivanavilesc <43003360+ivanavilesc@users.noreply.github.com>
2020-09-16 02:56:41 -0300

    EntityparaORACLE

---
+++ testAppRepo/Controllers/TipoProductoController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using testAppRepo.Models;
+namespace testAppRepo.Controllers
+{
+    public class TipoProductoController : Controller
+    {
+        private readonly CrudContext _context = new CrudContext();
+
+        // GET: Employees  
+        public ActionResult Index()
+        {
+            return View();
+        }
+    
+
+
+    // GET: TipoProducto
+    
+
+        public ActionResult Create()
+        {
+           
+            return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult Create(TIPOPRODUCTO objTipoProducto)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+
+
+                if (ModelState.IsValid)
+                {
+                    _context.TipoProducto.Add(objTipoProducto);
+                    _context.SaveChanges();
+                }
+                return View(objTipoProducto);
+
+
+                
+                
+                //return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+       
+    }
+}
---
+++ testAppRepo/Models/CrudContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace testAppRepo.Models
+{
+    public class CrudContext : DbContext
+    {
+        public DbSet<TIPOPRODUCTO> TipoProducto { get; set; }
+    }
+}
