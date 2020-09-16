fb4013cb50d7fd43525fba68166fd66f3ab0d85e
ivanavilesc <43003360+ivanavilesc@users.noreply.github.com>
2020-09-16 11:20:27 -0300

    CRUD con Scaffolding agregado

--- testAppRepo/Controllers/TipoProductoController.cs
+++ testAppRepo/Controllers/TipoProductoController.cs
@@ -1,60 +1,127 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using testAppRepo.Models;
+
 namespace testAppRepo.Controllers
 {
     public class TipoProductoController : Controller
     {
-        private readonly CrudContext _context = new CrudContext();
+        private RestaurantEntities db = new RestaurantEntities();
 
-        // GET: Employees  
+        // GET: TipoProducto
         public ActionResult Index()
         {
-            return View();
+            return View(db.TIPOPRODUCTO.ToList());
         }
-    
 
+        // GET: TipoProducto/Details/5
+        public ActionResult Details(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TIPOPRODUCTO tIPOPRODUCTO = db.TIPOPRODUCTO.Find(id);
+            if (tIPOPRODUCTO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tIPOPRODUCTO);
+        }
 
-    // GET: TipoProducto
-    
-
+        // GET: TipoProducto/Create
         public ActionResult Create()
         {
-           
             return View();
-
         }
 
+        // POST: TipoProducto/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        public ActionResult Create(TIPOPRODUCTO objTipoProducto)
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IDTIPOPRODUCTO,DESCTIPOPRODUCTO")] TIPOPRODUCTO tIPOPRODUCTO)
         {
-            try
+            if (ModelState.IsValid)
             {
-                // TODO: Add insert logic here
-
-
-
-                if (ModelState.IsValid)
-                {
-                    _context.TipoProducto.Add(objTipoProducto);
-                    _context.SaveChanges();
-                }
-                return View(objTipoProducto);
-
-
-                
-                
-                //return RedirectToAction("Index");
+                db.TIPOPRODUCTO.Add(tIPOPRODUCTO);
+                db.SaveChanges();
+                return RedirectToAction("Index");
             }
-            catch
-            {
-                return View();
-            }
+
+            return View(tIPOPRODUCTO);
         }
 
-       
+        // GET: TipoProducto/Edit/5
+        public ActionResult Edit(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TIPOPRODUCTO tIPOPRODUCTO = db.TIPOPRODUCTO.Find(id);
+            if (tIPOPRODUCTO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tIPOPRODUCTO);
+        }
+
+        // POST: TipoProducto/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IDTIPOPRODUCTO,DESCTIPOPRODUCTO")] TIPOPRODUCTO tIPOPRODUCTO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tIPOPRODUCTO).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tIPOPRODUCTO);
+        }
+
+        // GET: TipoProducto/Delete/5
+        public ActionResult Delete(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TIPOPRODUCTO tIPOPRODUCTO = db.TIPOPRODUCTO.Find(id);
+            if (tIPOPRODUCTO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tIPOPRODUCTO);
+        }
+
+        // POST: TipoProducto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(decimal id)
+        {
+            TIPOPRODUCTO tIPOPRODUCTO = db.TIPOPRODUCTO.Find(id);
+            db.TIPOPRODUCTO.Remove(tIPOPRODUCTO);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
     }
 }
